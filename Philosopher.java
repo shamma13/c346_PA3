296de4a0b360a1081aefadd435fab1d1ee20c38a
shamma13 <shammamarkis@hotmail.com>
2023-03-27 22:40:31 -0400

    implement philosopher class, fix deadlock

---
+++ Philosopher.java
@@ -0,0 +1,37 @@
+public class Philosopher extends BaseThread implements Runnable {
+    private final Object leftFork;
+    private final Object rightFork;
+
+    public Philosopher(Object leftFork, Object rightFork) {
+        this.leftFork = leftFork;
+        this.rightFork = rightFork;
+    }
+
+    private void doAction(String action) throws InterruptedException {
+        System.out.println(Thread.currentThread().getName() + " " + action);
+        Thread.sleep(((int) (Math.random() * 100)));
+    }
+
+    public void run() {
+        try {
+            while (true) {
+                // thinking
+                doAction(": Thinking");
+                synchronized (leftFork) {
+                    doAction(": Picked up left fork");
+                    synchronized (rightFork) {
+                        //eating
+                        doAction(": Picked up right fork - eating");
+
+                        doAction(": Put down right fork");
+                    }
+
+                    doAction(": Put down left fork. Back to thinking");
+                }
+            }
+        } catch(InterruptedException e) {
+            Thread.currentThread().interrupt();
+            return;
+        }
+    }
+}
