9fd7dc140f99a497a223db5457cecffa1ea8ac3c
shamma13 <shammamarkis@hotmail.com>
2023-03-29 04:11:20 -0400

    update Philosopher class

--- Philosopher.java
+++ Philosopher.java
@@ -1,37 +1,142 @@
-public class Philosopher extends BaseThread implements Runnable {
-    private final Object leftFork;
-    private final Object rightFork;
+/**
+ * Class Philosopher.
+ * Outlines main subrutines of our virtual philosopher.
+ *
+ * @author Serguei A. Mokhov, mokhov@cs.concordia.ca
+ */
+public class Philosopher extends BaseThread
+{
+    /**
+     * Max time an action can take (in milliseconds)
+     */
+    public static final long TIME_TO_WASTE = 1000;
 
-    public Philosopher(Object leftFork, Object rightFork) {
-        this.leftFork = leftFork;
-        this.rightFork = rightFork;
-    }
+    /**
+     * The act of eating.
+     * - Print the fact that a given phil (their TID) has started eating.
+     * - yield
+     * - Then sleep() for a random interval.
+     * - yield
+     * - The print that they are done eating.
+     */
+    public void eat()
+    {
+        try
+        {
+            System.out.println("Philosopher " + getTID() + " started eating.");
+            Thread.yield(); //give chance for other threads to yield
+            sleep((long)(Math.random() * TIME_TO_WASTE));
+            Thread.yield();
+            System.out.println("Philosopher " + getTID() + " done eating.");
 
-    private void doAction(String action) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName() + " " + action);
-        Thread.sleep(((int) (Math.random() * 100)));
-    }
-
-    public void run() {
-        try {
-            while (true) {
-                // thinking
-                doAction(": Thinking");
-                synchronized (leftFork) {
-                    doAction(": Picked up left fork");
-                    synchronized (rightFork) {
-                        //eating
-                        doAction(": Picked up right fork - eating");
-
-                        doAction(": Put down right fork");
-                    }
-
-                    doAction(": Put down left fork. Back to thinking");
-                }
-            }
-        } catch(InterruptedException e) {
-            Thread.currentThread().interrupt();
-            return;
+        }
+        catch(InterruptedException e)
+        {
+            System.err.println("Philosopher.eat():");
+            DiningPhilosophers.reportException(e);
+            System.exit(1);
         }
     }
+
+    /**
+     * The act of thinking.
+     * - Print the fact that a given phil (their TID) has started thinking.
+     * - yield
+     * - Then sleep() for a random interval.
+     * - yield
+     * - The print that they are done thinking.
+     */
+    public void think()
+    {
+        try
+        {
+            System.out.println("Philosopher " + getTID() + " started thinking.");
+            Thread.yield(); //give chance for other threads to yield
+            sleep((long)(Math.random() * TIME_TO_WASTE));
+            Thread.yield();
+            System.out.println("Philosopher " + getTID() + " done thinking.");
+
+        }
+        catch(InterruptedException e)
+        {
+            System.err.println("Philosopher.think():");
+            DiningPhilosophers.reportException(e);
+            System.exit(1);
+        }
+    }
+
+    /**
+     * The act of talking.
+     * - Print the fact that a given phil (their TID) has started talking.
+     * - yield
+     * - Say something brilliant at random
+     * - yield
+     * - The print that they are done talking.
+     */
+    public void talk()
+    {
+        System.out.println("Philosopher " + getTID() + " started talking.");
+        Thread.yield();
+        saySomething();
+        Thread.yield();
+        System.out.println("Philosopher " + getTID() + " is done talking.");
+
+    }
+
+    /**
+     * No, this is not the act of running, just the overridden Thread.run()
+     */
+    public void run()
+    {
+        for(int i = 0; i < DiningPhilosophers.DINING_STEPS; i++)
+        {
+            DiningPhilosophers.soMonitor.pickUp(getTID());
+
+            eat();
+
+            DiningPhilosophers.soMonitor.putDown(getTID());
+
+            think();
+
+            /*
+             * TODO:
+             * A decision is made at random whether this particular
+             * philosopher is about to say something terribly useful.
+             */
+            if(Math.random()>=0.5)
+            {
+                // Some monitor ops down here...
+                DiningPhilosophers.soMonitor.requestTalk();
+                talk();
+                DiningPhilosophers.soMonitor.endTalk();
+                // ...
+            }
+
+            Thread.yield();
+        }
+    } // run()
+
+    /**
+     * Prints out a phrase from the array of phrases at random.
+     * Feel free to add your own phrases.
+     */
+    public void saySomething()
+    {
+        String[] astrPhrases =
+                {
+                        "Eh, it's not easy to be a philosopher: eat, think, talk, eat...",
+                        "You know, true is false and false is true if you think of it",
+                        "2 + 2 = 5 for extremely large values of 2...",
+                        "If thee cannot speak, thee must be silent",
+                        "My number is " + getTID() + ""
+                };
+
+        System.out.println
+                (
+                        "Philosopher " + getTID() + " says: " +
+                                astrPhrases[(int)(Math.random() * astrPhrases.length)]
+                );
+    }
 }
+
+// EOF
